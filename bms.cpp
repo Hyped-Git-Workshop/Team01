1cb5dc0eede258a5515a8ad5d4dca3d192b8397f
vladrumyan <30479522+vladrumyan@users.noreply.github.com>
2017-11-18 15:35:44 +0000

    Add get_voltage function

--- bms.cpp
+++ bms.cpp
@@ -5,8 +5,13 @@ BatteryManagementSystem::BatteryManagementSystem()
 }
 
 /// TASK 9 (BOTH) //////////////////////////////////////////////////////////////
+
 double BatteryManagementSystem::get_current()
 {
-  return 0;
+	return 10000;
+}
+double BatteryManagementSystem::get_voltage()
+{
+  return 20000;
 }
 /// END OF TASKS 9AB ///////////////////////////////////////////////////////////
